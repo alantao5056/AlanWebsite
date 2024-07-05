4a68d5d9fb9804b0bf1722a3a64c1402fca24a90
alantao5056 <alantaolr@outlook.com>
2024-07-05 18:47:20 -0400

    changed a tag to Link tag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" to="/">
             <img className="logo" src="../images/alan-cropped.png"/>
-          </a>
+          </Link>
 
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -16,19 +17,19 @@ export default function Navbar() {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav w-100">
               <li className="nav-item ms-3">
-                <a href="/" className="nav-link">Home</a>
+                <Link to="/" className="nav-link">Home</Link>
               </li>
               <li className="nav-item ms-3">
-                <a href="/about" className="nav-link">About</a>
+                <Link to="/about" className="nav-link">About</Link>
               </li>
               <li className="nav-item ms-3">
-                <a href="/blog" className="nav-link">Blog</a>
+                <Link to="/blog" className="nav-link">Blog</Link>
               </li>
               <li className="nav-item" id="youtube">
-                <a href="https://www.youtube.com/@AlanCS-ub9id" target="_blank"><img id="youtube-logo" src="../images/youtube.png"/></a>
+                <Link to="https://www.youtube.com/@AlanCS-ub9id" target="_blank"><img id="youtube-logo" src="../images/youtube.png"/></Link>
               </li>
               <li className="nav-item" id="github">
-                <a href="https://github.com/alantao5056" target="_blank"><img id="github-logo" src="../images/github.png"/></a>
+                <Link to="https://github.com/alantao5056" target="_blank"><img id="github-logo" src="../images/github.png"/></Link>
               </li>
             </ul>
           </div>
