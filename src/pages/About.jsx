1b5e3eb87ecaab2768e82bcd48c9f5245c6a4fe9
alantao5056 <alantaolr@outlook.com>
2025-08-25 21:49:48 -0400

    fixed about and added more projects

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import Footer from "../components/Footer"
 export default function About() {
   return (
     <div className="about">
-      <div className="inner">
+      <div className="inner mb-5">
         <h1 className="display-3 mt-4 mb-3 text-primary">About Me</h1>
         {/* <div className="d-flex flex-wrap pe-3 w-100">
           
@@ -13,7 +13,7 @@ export default function About() {
         <h1 className="mt-5 text-primary">Programming</h1>
         <div className="about-card d-flex flex-wrap justify-content-left">
           <p className="ms-5 me-5">
-            I started learning programming when I was in 4th grade. My dad was able to teach me the basics because he is a programmer. After doing lots of practices with easy problems on LeetCode, I was able to learn more advanced data structures and languages. Now, I use programming languages such as C++, Java, and Python for competitive programming contests like <a href="https://usaco.org" target="_blank">USACO</a>, and R for data science. I'm currently trying to pass USACO Gold. I'm still new to web development and React, so this website might not be the prettiest. Because I love programming so much, I became the captain for programming club, and I even posted videos explaining programming problems. All my programming projects are public on my <a href="https://github.com/alantao5056" target="_blank">GitHub</a> page. 
+            I started learning the basics of programming from my dad when I was in 4th grade. After doing lots of practices with easy problems on LeetCode, I was able to learn more advanced data structures and languages. Now, I use programming languages such as C++, Java, and Python for competitive programming contests like <a href="https://usaco.org" target="_blank">USACO</a>, R for data science, Q# for quantum computing, and swift for app development. I'm still relatively new to web development and React, so this website might not be the prettiest. Because I love programming so much, I became the captain for programming club and created a <a href="https://nnhsprogramming.club" target="_blank">club website</a> for us as well, and I even posted videos explaining programming problems. All my programming projects are public on my <a href="https://github.com/alantao5056" target="_blank">GitHub</a> page. 
           </p>
           <img src="../images/programming.jpeg" alt="" width="300px" className="me-auto ms-auto"/>
         </div>
@@ -21,7 +21,7 @@ export default function About() {
         <h1 className="mt-5 text-primary">YouTube</h1>
         <div className="about-card d-flex flex-wrap justify-content-left">
           <p className="ms-5 me-3 mb-4 mt-3">
-            I have a <a href="https://www.youtube.com/channel/UCzdtyvYuUM6EGKhtsbF_jhQ" target="_blank">YouTube Channel</a> that I started on November 10th, 2023. I started by posting solutions to USACO Silver problems. However, it didn't get much attention, as USACO is not yet very well known, and people on YouTube don't enjoy these types of content as much. So, I experimented with posting ukulele covers of songs. The first cover I posted was <a href="https://www.youtube.com/watch?v=SN_2eu-3reo" target="_blank">IDGAF by BoyWithUke ft. blackbear.</a>. Although I don't think this was my best performance, it still got over 2.3k views, making it the most popular video on my channel. After this, I made more ukulele cover videos and also posted other things like badminton montages, and also a celebration for 100 subscribers. I hope I will post more creative videos in the future, and grow my channel. 
+            I have a <a href="https://www.youtube.com/channel/UCzdtyvYuUM6EGKhtsbF_jhQ" target="_blank">YouTube Channel</a> that I started on November 10th, 2023. I started by posting solutions to USACO Silver problems. However, it didn't get much attention, as USACO is not yet very well known, and people on YouTube don't enjoy these types of content as much. So, I experimented with posting ukulele covers of songs. The first cover I posted was <a href="https://www.youtube.com/watch?v=SN_2eu-3reo" target="_blank">IDGAF by BoyWithUke ft. blackbear.</a>. Quickly after, I posted <a href="https://www.youtube.com/watch?v=t19MRxlTntg">She Said No by BoywithUke</a>, getting over 10k views. I also posted other random things, like going to Costco and badminton montages. I hope to continue posting more content in the future and grow my channel even more. 
           </p>
           <iframe width="350px" height="200px" src="https://www.youtube.com/embed/SN_2eu-3reo?si=gHnEa18GpOn4qvjV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen className="me-auto ms-auto"></iframe>
         </div>
@@ -29,16 +29,17 @@ export default function About() {
         <h1 className="mt-5 text-primary">Badminton</h1>
         <div className="about-card d-flex flex-wrap justify-content-left">
           <p className="ms-5 me-3 mb-4 mt-3">
-            I started playing badminton when I was in 7th grade. I actually initially played table tennis, but I switched because I'm a bit too tall for it, which created problems for my posture. In contrast, I found that badminton is a sport where players have to straigten up and extend their body. This improved my posture, and I stuck with it. I won many tournaments, volunteered at the badminton course at Newton Parks and Recreation, and also was the captain of the badminton club at our school. Overall, this sport helped me stay active and in shape. 
+            I started playing badminton when I was in 7th grade. I actually initially played table tennis, but I switched because I'm a bit too tall for it, which created problems for my posture. Badminton, on the other hand, is a sport where players have to straigten up and extend their body. This improved my posture, and I stuck with it until today. I won many tournaments, volunteered at badminton camps at Newton Parks and Recreation, and also was the captain of the badminton club at our school. Overall, this sport helped me stay active and in shape. 
           </p>
-          <iframe width="350px" height="200px" src="https://www.youtube.com/embed/AlfUuBRpzMI?si=m0f-fLNBgWn-3JhK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen className="me-auto ms-auto"></iframe>
+          <iframe width="350px" height="200px" src="https://www.youtube.com/embed/JCSy6fdkxv4?si=gB7D0PQ1Nf9e7ts6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen className="me-auto ms-auto"></iframe>
         </div>
 
         <h1 className="mt-5 text-primary">Viola</h1>
         <div className="about-card d-flex flex-wrap justify-content-left">
-          <p className="ms-5 me-3 mb-5 mt-3 w-100">
-            I chose viola as my instrument in 5th grade. I played in the elementry school orchestra, which was very boring to me because the pieces were all very simple accompaniments. So I stopped going to the orchestra in middle school and didn't practice viola. I realized it was a big mistake in high school, because I remembered that I was actually decent at it. So I began taking private lessons and relearning. I now have joined the Junior Repertory Orchestra at BYSO. 
+          <p className="ms-5 me-3 mb-5 mt-3">
+            I chose viola as my instrument in 5th grade because of it's deep and warm tone. I played in the elementry school orchestra, which was very boring to me because the pieces were all very simple accompaniments. So I stopped going to the orchestra in middle school and didn't practice viola. But then I realized it was a big mistake in high school, because I had found my passion for music and I wanted to play in a more advanced orchestra again. So I began picking it up again. Currently I am in the Reportory Orchestra at Boston Youth Symphony Orchestras (BYSO).
           </p>
+          <iframe width="350px" height="200px" src="https://www.youtube.com/embed/ceofxYIgnMI?si=QGBvwbkWiLAmJFaE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen className="me-auto ms-auto"></iframe>
         </div>
       </div>
       <Footer />
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,49 +1,57 @@
 import Footer from "../components/Footer"
 
+
 export default function Projects() {
+  const projectCards = [
+    {
+      image: "../images/programming-club.png",
+      title: "Programming Club Website",
+      description: "The official website for the programming club at Newton North High School. Built with React and hosted on GitHub Pages.",
+      link: "https://nnhsprogramming.club"
+    },
+    {
+      image: "../images/spotify-songs.png",
+      title: "Spotify Songs",
+      description: "This is a project where I used R to analyze trends in spotify songs. I used datasets from Kaggle with different variables about a certain track (eg. Danceability, Popularity, Speechiness). ",
+      link: "https://alienjao.quarto.pub/spotify-songs/pages/"
+    },
+    {
+      image: "../images/carbon-dioxide.png",
+      title: "Carbon Dioxide",
+      description: "This is a project where I used R to analyze trends in carbon dioxide emissions. I used datasets from Kaggle.",
+      link: "https://alienjao.quarto.pub/carbon-dioxide/"
+    },
+    {
+      image: "../images/star-shopper.png",
+      title: "Star Shopper",
+      description: "A destop tool built with .NET Framework using C#. Used to help people know exactly where items are in a store (currently only Star and Shaws). This is very helpful to instacarters.",
+      link: "https://github.com/taojianzzh/StarShopper/tree/main"
+    },
+    {
+      image: "../images/namedle.png",
+      title: "Namedle",
+      description: "A fun twist on the popular game Wordle, where players guess names instead of words. Built with C++.",
+      link: "https://github.com/alantao5056/Namedle"
+    }
+  ];
+
   return (
     <div className="projects">
       <div className="inner">
         <h1 className="display-3 text-primary mt-4">Projects</h1>
-        <div className="d-flex flex-wrap w-100 p-5">
-          <div className="card me-3 mb-4" style={{width: "18rem"}}>
-            <a href="https://alienjao.quarto.pub/spotify-songs/pages/" target="_blank">
-              <img src="../images/spotify-songs.png" className="card-img-top" />
-            </a>
-            <div className="card-body d-flex flex-column">
-              <h5 className="card-title">Spotify Songs</h5>
-              <p className="card-text">
-                This is a project where I used R to analyze trends in spotify songs. I used datasets from Kaggle with different variables about a certain track (eg. Danceability, Popularity, Speechiness). 
-              </p>
-              <a href="https://alienjao.quarto.pub/spotify-songs/pages/" target="_blank" className="btn btn-primary mt-auto p-1">Visit Site</a>
+        <div className="d-flex flex-wrap w-100 p-5 justify-content-center">
+          {projectCards.map((card, idx) => (
+            <div className="card me-4 mb-5" style={{width: "18rem"}} key={idx}>
+              <a href={card.link} target="_blank" rel="noopener noreferrer">
+                <img src={card.image} className="card-img-top" alt={card.title} />
+              </a>
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title">{card.title}</h5>
+                <p className="card-text">{card.description}</p>
+                <a href={card.link} target="_blank" rel="noopener noreferrer" className="btn btn-primary mt-auto p-1">Visit Site</a>
+              </div>
             </div>
-          </div>
-
-          <div className="card me-3 mb-4" style={{width: "18rem"}}>
-            <a href="https://alienjao.quarto.pub/carbon-dioxide/" target="_blank">
-              <img src="../images/carbon-dioxide.png" className="card-img-top" />
-            </a>
-            <div className="card-body d-flex flex-column">
-              <h5 className="card-title">Carbon Dioxide</h5>
-              <p className="card-text">
-                This is a project where I used R to analyze trends in carbon dioxide emissions. I used datasets from Kaggle. 
-              </p>
-              <a href="https://alienjao.quarto.pub/carbon-dioxide/" target="_blank" className="btn btn-primary mt-auto p-1">Visit Site</a>
-            </div>
-          </div>
-
-          <div className="card me-3 mb-4" style={{width: "18rem"}}>
-            <a href="https://github.com/taojianzzh/StarShopper/tree/main" target="_blank">
-              <img src="../images/star-shopper.png" className="card-img-top" />
-            </a>
-            <div className="card-body d-flex flex-column">
-              <h5 className="card-title">Star Shopper</h5>
-              <p className="card-text">
-                A destop tool built with .NET Framework using C#. Used to help people know exactly where items are in a store (currently only Star and Shaws). This is very helpful to instacarters. 
-              </p>
-              <a href="https://github.com/taojianzzh/StarShopper/tree/main" target="_blank" className="btn btn-primary mt-auto p-1">Visit Site</a>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       <Footer />
